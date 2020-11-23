00c43b222fe2700932493e5f19483e0526c817f5
AnneCharlotteMichel <71698492+AnneCharlotteMichel@users.noreply.github.com>
2020-11-23 22:39:26 +0100

    Delete feature_count.sh

--- feature_count.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget ftp://ftp.ensembl.org/pub/release-101/gtf/homo_sapiens/Homo_sapiens.GRCh38.101.chr.gtf.gz
-gzip -d  Homo_sapiens.GRCh38.101.chr.gtf.gz
-
-singularity pull docker://evolbioinfo/subread:v2.0.1  
-singularity run subread_v2.0.1.sif -p -t gene -g gene_id -s 0 -a Homo_sapiens.GRCh38.101.chr.gtf -o output.counts *.bam
